bd48c23ee1e056f7ae3dba7b30ac9a4af6d15a18
Stemra14 <mstering12@gmx.at>
2018-01-25 14:37:42 +0100

    Initial commit

---
+++ Bot01_Test-src (1)/Dialogs/EchoDialog.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Threading.Tasks;
+
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Net.Http;
+using System.Net;
+
+namespace Microsoft.Bot.Sample.SimpleEchoBot
+{
+    [Serializable]
+    public class EchoDialog : IDialog<object>
+    {
+        protected int count = 1;
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+            var intxt = message.Text;
+            string responseString = string.Empty;
+
+            var query = intxt; //User Query
+            var knowledgebaseId = "d8390424-deb0-42f8-917d-53ecca600c1f"; // Use knowledge base id created.
+            var qnamakerSubscriptionKey = "7ab09a2bf68040f3b8d9aabcae64ea2a"; //Use subscription key assigned to you.
+
+            //Build the URI
+            Uri qnamakerUriBase = new Uri("https://westus.api.cognitive.microsoft.com/qnamaker/v1.0");
+            var builder = new UriBuilder($"{qnamakerUriBase}/knowledgebases/{knowledgebaseId}/generateAnswer");
+
+            //Add the question as part of the body
+            var postBody = $"{{\"question\": \"{query}\"}}";
+
+            //Send the POST request
+            using (WebClient client = new WebClient())
+            {
+                //Set the encoding to UTF8
+                client.Encoding = System.Text.Encoding.UTF8;
+
+                //Add the subscription key header
+                client.Headers.Add("Ocp-Apim-Subscription-Key", qnamakerSubscriptionKey);
+                client.Headers.Add("Content-Type", "application/json");
+                responseString = client.UploadString(builder.Uri, postBody);
+                await context.PostAsync(responseString.Split('"')[3]);
+            }
+        }
+
+        public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
+        {
+            var confirm = await argument;
+            if (confirm)
+            {
+                this.count = 1;
+                await context.PostAsync("Reset count.");
+            }
+            else
+            {
+                await context.PostAsync("Did not reset count.");
+            }
+            context.Wait(MessageReceivedAsync);
+        }
+
+    }
+}
---
+++ Bot01_Test-src (1)/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=bot01test-Web-Deploy.pubxml -p:Password=K8gbm8ZwKckKoo7k4ZQ2Bl6j1nbgS7d5QFnct2dR2c3giJbHmuytk3E9ta3A
+
